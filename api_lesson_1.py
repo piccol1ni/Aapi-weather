90fafa1b215495587fe85487b5e8e17495b4b4f6
Daniil <120402560+piccol1ni@users.noreply.github.com>
2022-12-26 15:52:45 +0300

    add some new fixes

--- api_lesson_1.py
+++ api_lesson_1.py
@@ -1,10 +1,10 @@
 import requests
 
 
-places = ['Лондон', 'Шереметьево', 'Череповец']
-
 def main():
-    payloads = {
+    places = ['Лондон', 'Шереметьево', 'Череповец']
+
+    payload = {
         'lang' : 'ru',
         'nTqM' : '',
     }
@@ -12,10 +12,11 @@ def main():
     for place in places:
         url = 'https://wttr.in/{}'.format(place)
 
-        response = requests.get(url, params=payloads)
+        response = requests.get(url, params=payload)
         response.raise_for_status()
 
         print(response.text)
+
 
 if __name__ == '__main__':
     main()
